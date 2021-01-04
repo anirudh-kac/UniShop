892003f37180c41c3cc48acdbfe2c9ed33c1875f
Anirudh Kachroo <anirudh.kac@yahoo.com>
2021-01-04 14:54:01 +0530

    register models

--- dbmsproject/unishop/admin.py
+++ dbmsproject/unishop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Store,Product,CartItem,Bill,UserProfile
 # Register your models here.
+
+admin.site.register(Store)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Bill)
+admin.site.register(UserProfile)

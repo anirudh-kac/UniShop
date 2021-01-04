c163cc28fc12c3590a55fd4061f6622256970853
Anirudh Kachroo <anirudh.kac@yahoo.com>
2021-01-04 14:52:09 +0530

    add urls

--- dbmsproject/unishop/urls.py
+++ dbmsproject/unishop/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
      path("",views.home,name = "home"),
+     path('login/',views.login_user,name = "login"),
+     path('logout/',views.logout_user,name = "logout"),
+     path('register/',views.register_user,name = "register"),
+     path('myshop/',views.myshop,name = "myshop"),
+     path('product/delete/<int:id>/',views.delete_product,name = 'delete_product'),
+     path('product/add/',views.add_product,name = "add_product"),
+     path('shops/',views.shops,name = "shops"),
+     path('shops/<int:id>/',views.shop,name = "shop"),
+     path('cart/',views.cart,name = "cart"),
+     path('product/addtocart/<int:id>',views.add_to_cart,name = "add_to_cart"),
+     path('order/',views.order,name = "order"),
  ]
  
